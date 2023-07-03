5a869ce62cac2bc1df20779cde0c96f48b0d7bc6
ciatadelle <138496019+ciatadelle@users.noreply.github.com>
2023-07-03 16:26:10 +0200

    Update median.js

--- src/utils/median.js
+++ src/utils/median.js
@@ -2,7 +2,7 @@ const median = (arr) => {
   const nums = arr.map(parseFloat).sort((a, b) => b - a); // Sort in descending order
   const topTen = nums.slice(0, 10); // Get the top 10 largest numbers
   const sum = topTen.reduce((acc, num) => acc + num, 0); // Calculate the sum of the top 10 numbers
-  return sum / topTen.length; // Calculate the average
+  return sum / 10; // Calculate the average of the top 10 numbers
 };
 
 module.exports = median;
